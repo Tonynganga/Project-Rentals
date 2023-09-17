b3027a55de05fa62b39eab2e0f53b4c8cfc90d06
TonyNganga <ubuntu@ip-172-31-89-131.ec2.internal>
2023-09-17 18:57:40 +0000

    Houses Context-V1

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ import Logo from '../assets/img/logo.svg';
 
 const Header = () =>{
     return(
-        <header className="py-6 mb-12 border-b">
+        <header className="py-4 mb-6 border-b">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to='/'>
                     <img src={Logo} alt='' />
@@ -17,7 +17,7 @@ const Header = () =>{
                     <Link className="hover:text-violet-900 transition" to='/'>
                         Log In
                     </Link>
-                    <Link className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-4 rounded-lg transition"
+                    <Link className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-2 rounded-lg transition"
                             to='/'>
                         Sign Up
                     </Link>
