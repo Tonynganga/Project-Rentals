25344c924b4fe63c986a263a17b452837abef467
TonyNganga <ubuntu@ip-172-31-89-131.ec2.internal>
2023-09-17 12:28:14 +0000

    First commit-Home page(Uncompleted)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+
+//pages
+import Home from './pages/Home';
+
+const App = () => {
+  return(
+    <div className="max-w-[1440px] mx-auto bg-white">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+
     </div>
   );
-}
+};
+
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//pages
+import Banner from '../components/Banner';
+
+const Home = () =>{
+    return (
+        <div className='min-h-[1800px'>
+            <Banner />
+        </div>
+    )
+}
+
+export default Home;
