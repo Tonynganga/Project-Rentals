20ea21938512ed1e165c2ef601741177ce7160d7
TonyNganga <ubuntu@ip-172-31-89-131.ec2.internal>
2023-09-17 13:01:56 +0000

    Header Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,15 @@ const Header = () =>{
                 <Link to='/'>
                     <img src={Logo} alt='' />
                 </Link>
+                <div className="flex items-center gap-6">
+                    <Link className="hover:text-violet-900 transition" to='/'>
+                        Log In
+                    </Link>
+                    <Link className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-4 rounded-lg transition"
+                            to='/'>
+                        Sign Up
+                    </Link>
+                </div>
             </div>
 
         </header>
